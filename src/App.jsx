ee4d144de86867d34bb355e536c46851ad3f1f65
Rasreal <ersultan.t16@gmail.com>
2024-10-03 18:44:54 +0500

    Name edit/save changes done

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ function App() {
       <div id="game-container">
         <ol id="players">
           
-          <Player name="1" symbol="X"/>
-          <Player name="2" symbol="O"/>
+          <Player name="Player 1" symbol="X"/>
+          <Player name="Player 2" symbol="O"/>
         </ol>
         GAME BOARD
       </div>
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,37 @@
-export default function Player({name, symbol}) {
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [isEdit, setEdit] = useState(false);
+  const [playerName, setPlayerName] = useState(name);
+  let editBtn = <button onClick={editBtnHandler}>Edit</button>;
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+  function editBtnHandler() {
+    setEdit(true);
+  }
+  function saveBtnHandler() {
+    setEdit(false);
+  }
+  function nameEditHandler(event){
+     console.log(event.target.value)
+     setPlayerName(event.target.value)
+  }
+
+  if (isEdit) {
+    editBtn = <button onClick={saveBtnHandler}>Save</button>;
+
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={nameEditHandler}></input>
+    );
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">Player {name}</span>
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      {editBtn}
     </li>
   );
 }
